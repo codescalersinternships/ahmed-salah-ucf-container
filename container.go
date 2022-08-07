494f8c220aad94099328a44f704c397c388ca8b4
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2022-08-07 20:08:01 +0200

    implement isolation of container from outside

--- container.go
+++ container.go
@@ -24,6 +24,8 @@ func main () {
 	}
 }
 
+// create new namespaces for the container,
+// and re-envoke the process with the child command in the new namespaces.
 func run() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
@@ -39,7 +41,21 @@ func run() {
 }
 
 func child() {
-	fmt.Printf("[%v] %v start running", os.Getpid(), os.Args[2:])
+	fmt.Printf("[%v] %v start running\n", os.Getpid(), os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	assert(syscall.Sethostname([]byte("container")))
+	assert(syscall.Chroot("./ubuntu-fs"))
+	assert(os.Chdir("/"))
+	assert(syscall.Mount("proc", "proc", "proc", 0, ""))
+
+	assert(cmd.Run())
+
+	assert(syscall.Unmount("/proc", 0))
 }
 
 func assert(err error) {
